8854cf3ad1a8ce4c258dc84cb0fd6dc9a06c5444
RAWAN <rawan@RAWANs-MacBook-Pro.local>
2018-09-27 01:41:08 +0300

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -25,16 +25,17 @@ cardImage: "images/king-of-diamonds.png"
 var cardsInPlay = [];
 
 var checkForMatch = function () {
+	this.setAttribute('src' , cards[cardId].cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 alert("You found a match!");
 } else {
 alert("Sorry, try again.");
  }
-}
+};
 
 
-var flipCard = function (cardId) {
-
+var flipCard = function () {
+this.getAttribute('data-id');
 cardsInPlay.push(cards[cardId].rank);
 console.log("User flipped" + ' ' + cards[cardId].rank);
 
@@ -45,6 +46,15 @@ console.log(cards[cardId].suit);
 checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
 
+
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++) {
+   var cardElement = document.createElement('img');
+   cardElement.setAttribute('src', "images/back.png");
+   cardElement.setAttribute('data-id', i);
+   cardElement.addEventListener('click' , flipCard);
+   cardElement.appendChild(game-board)
+}
+}
+createBoard();
